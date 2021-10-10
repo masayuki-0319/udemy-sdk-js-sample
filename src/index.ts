3ff3f7a0980dc643490f8acbf6f53af8d7cf10e0
将之大西 <omccello@gmail.com>
2021-10-10 18:18:39 +0900

    course-detail実行可能

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,40 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import { authorizationToken } from './config.js';
+import { CourseDetailResponse, CourseListResponse } from './types/response.js';
 
-const BASE_URL = 'https://www.udemy.com/api-2.0/courses/?page_size=1';
+const DOMAIN = 'https://www.udemy.com/';
+const VERSION = 'api-2.0';
+const BASE_URL = DOMAIN + VERSION;
 
-type CourseListResponse = {
-  [k: string]: any;
+const baseRequestConfig: AxiosRequestConfig = {
+  headers: {
+    Accept: 'application/json, text/plain, */*',
+    Authorization: `Basic ${authorizationToken}`,
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  baseURL: BASE_URL,
 };
 
-const response = async () => {
-  const response = await axios.get<CourseListResponse>(BASE_URL, {
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      Authorization: `Basic ${authorizationToken}`,
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-  });
+const COURSE_LIST_ENDPOINT = '/courses/?page_size=1' as const;
+const COURSE_DETAIL_ENDPOINT = '/courses/3197760' as const;
+
+const apiRequest = async <T extends CourseListResponse | CourseDetailResponse>(
+  endpoint: typeof COURSE_LIST_ENDPOINT | typeof COURSE_DETAIL_ENDPOINT
+) => {
+  const response = await axios.get<T>(endpoint, baseRequestConfig);
 
   return response;
 };
 
 const main = async () => {
-  const { status, statusText, data } = await response();
+  const { status, statusText, data } = await apiRequest<CourseDetailResponse>(
+    COURSE_DETAIL_ENDPOINT
+  );
   console.log('status: ', status);
   console.log('statusText: ', statusText);
   console.log('data: ', data);
+  console.log('-----------------');
 };
+
 main();
