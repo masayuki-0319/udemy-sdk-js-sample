d48369af294a6aa6acc935f1f28abfba4415814e
将之大西 <omccello@gmail.com>
2021-10-10 17:53:09 +0900

    axios によるgetリクエスト実装

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-console.log('ハロー１');
-console.log('ハロー２');
-console.log('ハロー３');
-console.log('ハロー４');
-console.log('ハロー５');
-console.log('ハロー６');
-console.log('ハロー７');
-console.log('ハロー８');
-console.log('ハロー９');
+import axios from 'axios';
+
+import { authorizationToken } from './config.js';
+
+const BASE_URL = 'https://www.udemy.com/api-2.0/courses/?page_size=1';
+
+type CourseListResponse = {
+  [k: string]: any;
+};
+
+const response = async () => {
+  const response = await axios.get<CourseListResponse>(BASE_URL, {
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      Authorization: `Basic ${authorizationToken}`,
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  });
+
+  return response;
+};
+
+const main = async () => {
+  const { status, statusText, data } = await response();
+  console.log('status: ', status);
+  console.log('statusText: ', statusText);
+  console.log('data: ', data);
+};
+main();
