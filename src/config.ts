c2d2247ab6e2878cc2449de80d0c56553f420827
将之大西 <omccello@gmail.com>
2021-10-16 22:48:28 +0900

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,22 @@
 import axios, { AxiosRequestConfig, AxiosStatic } from 'axios';
 import base64 from 'base-64';
 
-const UDEMY_AFFILIATE_CLIENT_ID = '';
-const UDEMY_AFFILIATE_CLIENT_PASSWORD = '';
-
-const authorizationToken = base64.encode(
-  `${UDEMY_AFFILIATE_CLIENT_ID}:${UDEMY_AFFILIATE_CLIENT_PASSWORD}`
-);
-
 export type AffiliateApiConfig = {
   client: AxiosStatic;
   cleintConfig: AxiosRequestConfig;
 };
-export const getConfig = (): AffiliateApiConfig => {
+
+export const getConfig = (
+  clientID: string,
+  clientPassword: string
+): AffiliateApiConfig => {
+  const authorization = base64.encode(`${clientID}:${clientPassword}`);
   return {
     client: axios,
     cleintConfig: {
       headers: {
         Accept: 'application/json, text/plain, */*',
-        Authorization: `Basic ${authorizationToken}`,
+        Authorization: `Basic ${authorization}`,
         'Content-Type': 'application/json;charset=utf-8',
       },
       baseURL: 'https://www.udemy.com/api-2.0',
